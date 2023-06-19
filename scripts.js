a1cbab510fa496d9b3ce5b9d5d25a11bcf08fa0b
Bomtek9 <clayduplantis9@gmail.com>
2023-06-19 14:44:54 -0500

    Pokemon List
    Created Pokemon list with name, height, and type

--- scripts.js
+++ scripts.js
@@ -1,7 +1,18 @@
-alert('Hello World')
+let pokemonList = [// database of pokemon for the pokedex
+{ 
+    name: 'Bulbasur', 
+    height: '7',
+    type: ['grass','poison']
+},
+{
+    name: 'Pikachu',
+    height: '12',
+    type: 'electric',
+},
+{
+    name: 'Charmander',
+    height: '6',
+    type: 'fire',
+}
 
-let favoriteFood = 'Crunchy Tacos';
-document.write(favoriteFood);
-
-let age = '39';
-document.write(age);
+]
