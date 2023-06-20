898b1008b6965093349c5dce3cfbb3fe7a8b1cf0
Bomtek9 <clayduplantis9@gmail.com>
2023-06-20 13:06:15 -0500

    Adjusted Array
    Type to Types

--- scripts.js
+++ scripts.js
@@ -2,17 +2,17 @@ let pokemonList = [// database of pokemon for the pokedex
 { 
     name: 'Bulbasur', 
     height: '7',
-    type: ['grass','poison']
+    types: ['grass','poison'],
 },
 {
     name: 'Pikachu',
     height: '12',
-    type: 'electric',
+    types: ['electric'],
 },
 {
     name: 'Charmander',
     height: '6',
-    type: 'fire',
+    types: ['fire'],
 }
 
 ]
