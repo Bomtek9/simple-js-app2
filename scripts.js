f75d1fd16692987a89216e08f933af93a94163a4
Bomtek9 <clayduplantis9@gmail.com>
2023-07-07 14:46:56 -0500

    For to forEach
    changed scripts for loop to IIFE

--- scripts.js
+++ scripts.js
@@ -1,38 +1,41 @@
-let turtleList = [// database of pokemon for the pokedex
-{ 
-    name: 'Leonardo', 
-    height: '7',
-    types: ['leader'],
-},
-{
-    name: 'Donatello',
-    height: '7.5',
-    types: ['brains'],
-},
-{
-    name: 'Raphael',
-    height: '7.2',
-    types: ['angry'],
-},
+let turtlesRepository = (function () {  //IIFE
 
-{
-  name: 'Michaelangelo',
-  height: '7.3',
-  types: ['funny'],
-}
-]
-
-for (let i = 0; i < turtleList.length; i++) {
-    if (turtleList[i].height < 7.1) {
-      document.write(
-        turtleList[i].name + ':' + ` (Height: ${turtleList[i].height})` + ' - I am the leader!'
-      );
-    } else {
-      document.write(
-        turtleList[i].name + ':' + ` (Height: ${turtleList[i].height})` //will type out other turtles not >7.1
-      );
+  let turtlesList = [   // database of turtles for the tmntdex
+    {
+      name: 'Leonardo', 
+      height: 7,
+      types: ['leader', 'blue'],
+    },
+    {
+      name: 'Donatello',
+      height: 7.5,
+      types: ['brains', 'purple'],
+    },
+    {
+      name: 'Raphael',
+      height: 7.2,
+      types: ['angry', 'red'],
+    },
+  
+    {
+      name: 'Michaelangelo',
+      height: 6.8,
+      types: ['funny', 'orange'],
     }
-    //adds a row space in-between each turtle entry
-    document.write('<br> <br>') 
+  ]
+
+  function getAll () {
+    return turtlesList;
+  } 
+  
+  function add (turtles) {
+    turtlesList.push(turtles);
   }
 
+  return {
+    getAll: getAll,
+    add: add
+  }
+})()
+
+document.write(turtlesRepository.getAll())
