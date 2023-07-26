76193f1a238b6af6881f43594fc4c78b49d0edf9
Bomtek9 <clayduplantis9@gmail.com>
2023-07-26 16:14:12 -0500

    fixing

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 let turtleRepository = (function() { //IIFE
-    let turtleList = [// database of turtles for the turtledex
+    let repository = [// database of turtles for the turtledex
   { 
     name: 'Leonardo', 
     height: '5.9',
@@ -20,47 +20,58 @@ let turtleRepository = (function() { //IIFE
   name: 'Michaelangelo',
   height: '5.6',
   types: ['funny'],
-  }
+  },
   ];
-  });
+
 
 
   function add(turtle) {
-    turtleList.push(turtle);
-    };
-
-
-  function getAll() {
-    return turtleList;
-    };
-
- //Put the item in the pokemonList onto the page
-  function addListItem(turtle) {
-    let turtleList = document.querySelector('.turtle-list');
-    let turtleListItem = document.createElement('li');
-
-  //Create button in the list item
-  let button = document.createElement('button');
-  button.classList.add('list-button');
-  button.innerText = turtle.name;
-  turtleListItem.appendChild(button);
-  turtleList.appendChild(turtleListItem);
-
-
-  button.addEventListener('click', function () {
-    showDetails(turtle);
-  });
-
+    if (
+      typeof turtle === 'object' &&
+      'name' in turtle &&
+      'height' in turtle &&
+      'types' in turtle
+    ) {
+      repository.push(turtle);
+    } else {
+      document.write("Turtle is not correct");
+    }
   }
 
+  function getAll() {
+    return repository;
+    }
+    function addListItem (turtle){
+      let turtleList = document.querySelector('.turtle-list');
+      let listTurtle = document.createElement('li');
+      let button = document.createElement('button');
+      button.innerText = turtle.name;
+      button.classList.add('list-button');
+      listTurtle.appendChild(button);
+      turtleList.appendChild(listTurtle);
+    }
+
+    return {
+      add: add,
+      getAll: getAll,
+      addListItem: addListItem,
+    };
+  })();
+
+  document.write(turtleRepository.getAll());
+
+  turtleRepository.getAll().forEach(function (turtle) {
+    pokemon.addListItem(turtle);
+
+}); 
+  
+
+  
 
 
 
 
 
 
-turtleRepositoryRepository.loadList().then(() => {
-  turtleRepositoryRepository.getAll().forEach((turtle) => {
-      turtleRepositoryRepository.addListItem(turtle);
-  });
-});
+
+
