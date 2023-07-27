8957fffb707b98e7476f93efc1e1a2ef1185139b
Bomtek9 <clayduplantis9@gmail.com>
2023-07-27 10:05:02 -0500

    Bug Fixes

--- scripts.js
+++ scripts.js
@@ -1,29 +1,27 @@
-var turtleRepository = (function() { //IIFE
-    let repository = [// database of turtles for the turtledex
-  { 
-    name: 'Leonardo', 
-    height: 5.9,
-    types: ['in charge'],
-  },
-  {
-    name: 'Donatello',
-    height: 6.6,
-    types: ['smart'],
-  },
-  {
-    name: 'Raphael',
-    height: 6,
-    types: ['angry'],
-  },
-
-  {
-  name: 'Michaelangelo',
-  height: 5.6,
-  types: ['funny'],
+var turtleRepository = (function() {
+  let repository = [
+    // database of turtles for the turtledex
+    {
+      name: 'Leonardo',
+      height: 5.9,
+      types: ['in charge'],
+    },
+    {
+      name: 'Donatello',
+      height: 6.6,
+      types: ['smart'],
+    },
+    {
+      name: 'Raphael',
+      height: 6,
+      types: ['angry'],
+    },
+    {
+      name: 'Michaelangelo',
+      height: 5.6,
+      types: ['funny'],
     },
   ];
-
-
 
   function add(turtle) {
     if (
@@ -34,37 +32,38 @@ var turtleRepository = (function() { //IIFE
     ) {
       repository.push(turtle);
     } else {
-      console.log.write("Turtle is not correct");
+      console.log("Turtle is not correct");
     }
   }
 
   function getAll() {
     return repository;
-    }
-    function addListItem (turtle){
-      let turtleList = document.querySelector('.turtle-list');
-      let listTurtle = document.createElement('li');
-      let button = document.createElement('button');
-      button.innerText = turtle.name;
-      button.classList.add('list-button');
-      listTurtle.appendChild(button);
-      turtleList.appendChild(listTurtle);
-      button.addEventListener("click", function (event) {
-        showDetails(turtle);
-        
-    });
-
-    return {
-      add: add,
-      getAll: getAll,
-      addListItem: addListItem,
-    };
   }
 
+  function addListItem(turtle) {
+    let turtleList = document.querySelector('.turtle-list');
+    let listTurtle = document.createElement('li');
+    let button = document.createElement('button');
+    button.innerText = turtle.name;
+    button.classList.add('list-button');
+    listTurtle.appendChild(button);
+    turtleList.appendChild(listTurtle);
+    button.addEventListener('click', function (event) {
+      showDetails(turtle);
+    });
+  }
+
+  return {
+    add: add,
+    getAll: getAll,
+    addListItem: addListItem,
+  };
+})();
+
+document.addEventListener('DOMContentLoaded', function () {
   console.log(turtleRepository.getAll());
 
   turtleRepository.getAll().forEach(function (turtle) {
-    pokemon.addListItem(turtle);
-
-}); 
+    turtleRepository.addListItem(turtle);
+  });
 });
